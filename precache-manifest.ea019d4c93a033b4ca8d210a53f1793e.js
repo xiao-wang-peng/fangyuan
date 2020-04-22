831bf0ceb68d7a9bb88b0785ab162b942ae2559b
王朋12 <5665754+wang_peng_12@user.noreply.gitee.com>
2020-04-22 22:51:47 +0800

    Updates

---
+++ precache-manifest.ea019d4c93a033b4ca8d210a53f1793e.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "00a6084c02dd3b8ceb4d0bd9ba0c20fe",
+    "url": "/fangyuan/index.html"
+  },
+  {
+    "revision": "f3e9ca83a01848899727",
+    "url": "/fangyuan/static/css/2.38aff980.chunk.css"
+  },
+  {
+    "revision": "fcafc5c6a42988087e72",
+    "url": "/fangyuan/static/css/main.78dde5dd.chunk.css"
+  },
+  {
+    "revision": "f3e9ca83a01848899727",
+    "url": "/fangyuan/static/js/2.9d2e4c26.chunk.js"
+  },
+  {
+    "revision": "4d47ffde3dc69784e8e23fd910817ef8",
+    "url": "/fangyuan/static/js/2.9d2e4c26.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "fcafc5c6a42988087e72",
+    "url": "/fangyuan/static/js/main.d82a16ab.chunk.js"
+  },
+  {
+    "revision": "e1bc965d4c3ec2dc5cac",
+    "url": "/fangyuan/static/js/runtime-main.19322fb2.js"
+  },
+  {
+    "revision": "3b538f76f922de087df0eb82f703119a",
+    "url": "/fangyuan/static/media/iconfont.3b538f76.ttf"
+  },
+  {
+    "revision": "80483e3b898177297ad8b08c1e002f55",
+    "url": "/fangyuan/static/media/iconfont.80483e3b.eot"
+  },
+  {
+    "revision": "c931e1ae5f45a521413c9905641f2071",
+    "url": "/fangyuan/static/media/iconfont.c931e1ae.svg"
+  },
+  {
+    "revision": "f58b610e9d4e6470d644d7d195bbfda6",
+    "url": "/fangyuan/static/media/iconfont.f58b610e.woff"
+  }
+]);
